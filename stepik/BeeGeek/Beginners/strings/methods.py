66175e436f1967d159453d8726370299a4a8e3a5
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-09-30 16:41:24 +0300

    tasks with methods of strings

---
+++ stepik/BeeGeek/Beginners/strings/methods.py
@@ -0,0 +1,26 @@
+s = 'foO BaR BAZ quX foo123#BAR#.'
+
+print(s.capitalize())  # 1 char capitalized
+print(s.swapcase())
+print(s.title())  # every 1 char of each word is capitalized
+print(s.lower())
+print(s.upper())
+
+s = 'i Learn Python language'
+print(s.capitalize())
+
+s = 'i LEARN Python LAnguaGE'
+print(s.lower())
+
+s = '$12344%^$#@!'
+print(s.lower())
+
+s1 = 'a'
+s2 = s1.upper()
+print(s1, s2)
+
+s = 'i LEARN Python LAnguaGE'
+print(s.upper())
+
+s = 'i LEARN Python LAnguaGE'
+print(s.swapcase())
