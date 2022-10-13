dff0af5932fcdbf04e91181533404311f6dd326a
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-10-13 15:08:56 +0300

    nested dictionaries generators

---
+++ stepik/BeeGeek/pre_pro/ditionaries/nested/ex5.py
@@ -0,0 +1,11 @@
+"""
+Дополните приведенный код, используя генератор, так, чтобы получить словарь result ,
+в котором ключом будет строка – элемент списка words, а значением –
+список соответствующих кодов ASCII символов данной строки.
+"""
+
+words = ['hello', 'bye', 'yes', 'no', 'python', 'apple', 'maybe', 'stepik', 'beegeek']
+
+result = {word:[ord(char) for char in word] for word in words}
+
+print(result)
