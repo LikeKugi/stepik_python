4e03f0f0fa4d737da189163315ce76aa3892afd7
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-10-14 15:03:51 +0300

    monte carlo and bogosort algorithms

---
+++ stepik/BeeGeek/pre_pro/monte_carlo_method/ex1.py
@@ -0,0 +1,13 @@
+import random
+
+n = 1000
+k = 0
+s0 = 16
+for _ in range(n):
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
+
+    if y**3 - 2*x**2 <= -1 and 2*y + x**3 <= 3:
+        k += 1
+
+print((k/n)*s0)
---
+++ stepik/BeeGeek/pre_pro/monte_carlo_method/finding_pi.py
@@ -0,0 +1,19 @@
+from random import uniform as ru
+import time
+
+n = 10 ** 12     # количество испытаний
+k = 0           # количество точек внутри фигуры
+s0 = 4         # площадь квадрата в котором фигура
+
+time_start = time.time()
+for _ in range(n):
+    x = ru(-1, 1)
+    y = ru(-1, 1)
+
+    if x**2 + y**2 <=1:
+        k+=1
+time_end = time.time()
+
+print(time_end - time_start)
+
+print((k/n)*s0)
