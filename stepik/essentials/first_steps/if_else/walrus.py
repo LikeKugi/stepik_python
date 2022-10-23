e10dcef06aa9854ad272fb2cdcddb0f2c75a15e6
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-10-23 16:46:01 +0300

    if else tasks

---
+++ stepik/essentials/first_steps/if_else/walrus.py
@@ -0,0 +1,9 @@
+print(see_walrus := 'Look at my walrus, my walrus is amazing')
+
+print(see_walrus[:11] + 'horse')
+
+
+if number := int(input('Введите число: ')) == 100:
+  print('Соточка')
+else:
+  print('Не Соточка')
