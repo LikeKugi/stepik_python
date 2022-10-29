7ea480df77cd1bdc828b115a6a07009e50aee67d
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-10-29 13:17:10 +0300

    decorators

---
+++ stepik/essentials/functions/decorators/repeater.py
@@ -0,0 +1,18 @@
+# Напишите декоратор repeater, который дважды вызывает декорированную функцию
+
+def repeater(func):
+
+    def inner(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+
+    return inner
+
+
+@repeater
+def multiply(num1, num2):
+    print(num1 * num2)
+
+
+multiply(2, 7)
+multiply(5, 3)
