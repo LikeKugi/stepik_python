0bab8abfa4bcbc3d250c57142b30a0abbd430ad0
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-11-10 15:47:11 +0300

    excel pandas

--- stepik/practice/datasheets/ex2/xlsx_sort.py
+++ stepik/practice/datasheets/ex2/xlsx_sort.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import csv
 
 df = pd.ExcelFile("data.xlsx").parse() # формируем dataframe
 
 df_sorted = df.sort_values(by=['ККал на 100', 'Unnamed: 0'], ascending=[False, True])
 
-print(df_sorted)
-df_top = df_sorted.head()
+print(*df_sorted['Unnamed: 0'].to_list(), sep='\n')
