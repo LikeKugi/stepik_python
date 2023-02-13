d21cbfb45484cce933c6cd56cca94ae841bb7df9
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-02-13 06:41:31 +0300

    binary tree

---
+++ stepik/structures/binary_tree.py
@@ -0,0 +1,121 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
+
+    def __repr__(self):
+        return f'{self.data}'
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def __find(self, node, parent, value):
+        if node is None:
+            return None, parent, False
+
+        if value == node.data:
+            return node, parent, True
+
+        if value < node.data:
+            if node.left:
+                return self.__find(node.left, node, value)
+
+        if value > node.data:
+            if node.right:
+                return self.__find(node.right, node, value)
+
+        return node, parent, False
+
+    def append(self, obj):
+        if self.root is None:
+            self.root = obj
+            return obj
+
+        s, p, fl_find = self.__find(self.root, None, obj.data)
+
+        if not fl_find and s:
+            if obj.data < s.data:
+                s.left = obj
+            else:
+                s.right = obj
+
+        return obj
+
+    def show_tree(self, node):
+        if node is None:
+            return
+
+        self.show_tree(node.left)
+        print(node.data, end=" ")
+        self.show_tree(node.right)
+
+    def show_wide_tree(self, node):
+        if node is None:
+            return
+
+        _children = []
+        v = [node]
+        while v:
+            vn = []
+            for x in v:
+                if x.right:
+                    vn += [x.right]
+                if x.left:
+                    vn += [x.left]
+            _children.extend(v)
+            v = vn
+
+        print(*_children[::-1])
+
+    def __del_leaf(self, s, p):
+        if p.left == s:
+            p.left = None
+        elif p.right == s:
+            p.right = None
+
+    def __del_one_child(self, s, p):
+        if p.left == s:
+            if s.left is None:
+                p.left = s.right
+            elif s.right is None:
+                p.left = s.left
+        elif p.right == s:
+            if s.left is None:
+                p.right = s.right
+            elif s.right is None:
+                p.right = s.left
+
+    def __find_min(self, node, parent):
+        if node.left:
+            return self.__find_min(node.left, node)
+
+        return node, parent
+
+    def del_node(self, key):
+        s, p, fl_find = self.__find(self.root, None, key)
+
+        if not fl_find:
+            return None
+
+        if s.left is None and s.right is None:
+            self.__del_leaf(s, p)
+        elif s.left is None or s.right is None:
+            self.__del_one_child(s, p)
+        else:
+            sr, pr = self.__find_min(s.right, s)
+            s.data = sr.data
+            self.__del_one_child(sr, pr)
+
+
+def main():
+    tree = Tree()
+
+    for x in (10, 5, 7, 16, 13, 2, 20):
+        tree.append(Node(x))
+
+    tree.show_wide_tree(tree.root)
+
+if __name__ == '__main__':
+    main()
